c6736ed3d617b48fdedc49b2d9a1133fddc6416e
Nir Mizrahi <kopatch@gmail.com>
2018-12-26 17:53:21 +0200

    updated naming

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
 import playerOptions from '../config/playerOptions.json';
-import animatedButton from './spritesheetButton';
+import spritesheetButton from './spritesheetButton';
 
 
 export default {
     onLoad: function(ctx) { },
 
     onInit: function(player, ctx) {
-        player.ui.override(['button_beginning_e3bdbd', 'button_beginning_34a305', 'button_beginning_6c2e1e'], animatedButton);
+        player.ui.override(['button_beginning_e3bdbd', 'button_beginning_34a305', 'button_beginning_6c2e1e'], spritesheetButton);
     },
 
     playerOptions
