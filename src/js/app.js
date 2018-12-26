baff49e8bc962e7ca50cb0efa75b0b9acb1d9bc6
Nir Mizrahi <kopatch@gmail.com>
2018-12-26 17:13:36 +0200

    animatedButton opacity

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@ export default {
     onLoad: function(ctx) { },
 
     onInit: function(player, ctx) {
-        player.ui.override('button_beginning_e3bdbd', animatedButton);
+        player.ui.override(['button_beginning_e3bdbd', 'button_beginning_34a305', 'button_beginning_6c2e1e'], animatedButton);
     },
 
     playerOptions
